df2952f09450603263f70831eb61a6720c010914
Chernenkiy <nikmbogdanov@gmail.com>
2021-12-03 13:41:29 +0300

    Useless code deleted

--- tasks_manager.py
+++ tasks_manager.py
@@ -21,7 +21,7 @@ class TasksManager:
             task.make_important()
             return
         if get_current_screen().name == "my_day":
-            task.set_deadline(datetime.date.today().strftime("%Y-%m-%d"))
+            task.deadline = datetime.date.today().strftime("%Y-%m-%d")
         self.add_task_to_screen(-1, get_screen_manager().current)
 
     def add_task_to_screen(self, task_id, screen_name):
